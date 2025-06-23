cc4dfb20114f2a3c88ca5ee55aea6007a11e15ca
Vallérian Dicque <83017594+cidevalgames@users.noreply.github.com>
2025-06-23 22:07:51 +0200

    Stylized all the UI of the Story Creator scene

--- Assets/Scripts/CreateStory/LineRendererHandler.cs
+++ Assets/Scripts/CreateStory/LineRendererHandler.cs
@@ -7,6 +7,8 @@ namespace StoryEditor
 {
     public class LineRendererHandler : MonoBehaviour
     {
+        private Button _openingButton, _closingButton;
+
         private Vector2 _openingButtonPosition;
         private Vector2 _closingButtonPosition;
         private LineRenderer _lineRenderer;
@@ -22,22 +24,27 @@ namespace StoryEditor
             _lineRenderer.GetComponent<LineRenderer>().positionCount = 2;
             _lineRenderer.SetPosition(0, clickedButton.transform.position);
 
-            _openingButtonPosition = clickedButton.transform.position;
+            _openingButton = clickedButton;
         }
 
         public void CloseLink(Button clickedButton)
         {
             _lineRenderer.GetComponent<LineRenderer>().SetPosition(1, clickedButton.transform.position);
-            _closingButtonPosition = clickedButton.transform.position;
+            _closingButton = clickedButton;
+            
             _isLinked = true;
         }
 
         private void Update()
         {
+            _openingButtonPosition = _openingButton.transform.position;
+
             _lineRenderer.SetPosition(0, _openingButtonPosition);
 
             if (_isLinked)
             {
+                _closingButtonPosition = _closingButton.transform.position;
+
                 _lineRenderer.SetPosition(1, _closingButtonPosition);
             }
         }
